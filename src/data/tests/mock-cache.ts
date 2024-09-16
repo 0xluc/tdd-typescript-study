09ca1ad716a9fe96b7a57188b571ea71af62dff6
Lucas <lucasbrodrigues14@gmail.com>
2024-09-16 21:08:32 +0000

    ensure LocalLoadPurchases calls cacheStore with correct key

--- src/data/tests/mock-cache.ts
+++ src/data/tests/mock-cache.ts
@@ -5,7 +5,12 @@ export class CacheStoreSpy implements CacheStore {
     actions: Array<CacheStoreSpy.Action> = [];
     deleteKey: string;
     insertKey: string;
+    fetchKey: string;
     insertValues: Array<SavePurchases.Params> = [];
+    fetch(key: string): void {
+        this.actions.push(CacheStoreSpy.Action.fetch);
+        this.fetchKey = key;
+    }
     delete(key: string): void {
         this.actions.push(CacheStoreSpy.Action.delete);
         this.deleteKey = key;
@@ -41,5 +46,6 @@ export namespace CacheStoreSpy {
     export enum Action {
         delete,
         insert,
+        fetch,
     }
 }
---
+++ src/domain/usecases/load-purchases.ts
@@ -0,0 +1,7 @@
+import { PurchaseModel } from "@/domain/models";
+export interface LoadPurchases {
+    loadAll: () => Promise<Array<LoadPurchases.Result>>;
+}
+export namespace LoadPurchases {
+    export type Result = PurchaseModel;
+}
